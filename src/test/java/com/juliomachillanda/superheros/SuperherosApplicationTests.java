7ddc1474411b629b7ccb17740bf8c7aeacbb8a0d
juliomachillanda1 <juliomachillanda1@gmail.com>
2023-03-03 10:47:33 -0300

    configuracion inicial

---
+++ src/test/java/com/juliomachillanda/superheros/SuperherosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juliomachillanda.superheros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperherosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
