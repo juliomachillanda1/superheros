fd676e963513c89a9077b750d1f42e53e2822036
juliomachillanda1 <juliomachillanda1@gmail.com>
2023-03-09 18:49:15 -0300

    Se agrega el esquema y la query para el manejo de las pruebas db con el H2 embebido

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
 INSERT INTO superheroes (name, superpower) VALUES
   ('Superman', 'Super strength, speed, and flight'),
   ('Wonder Woman', 'Super strength, agility, and Lasso of Truth'),
-  ('Spider-Man', 'Web-slinging and spider-like abilities');
+  ('Spiderman', 'Web-slinging and spider-like abilities');
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,5 @@
+CREATE TABLE superheroes (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  superpower VARCHAR(255) NOT NULL
+);
