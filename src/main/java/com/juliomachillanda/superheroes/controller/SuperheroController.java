370834b3e0675d35d0fc74934e1f9aec27ccc6a8
juliomachillanda1 <juliomachillanda1@gmail.com>
2023-03-09 19:13:10 -0300

    Servicio RestController con entity y advice para el manejo de las excepciones centralizadas

--- src/main/java/com/juliomachillanda/superheroes/controller/SuperheroController.java
+++ src/main/java/com/juliomachillanda/superheroes/controller/SuperheroController.java
@@ -1,47 +1,50 @@
 package com.juliomachillanda.superheroes.controller;
 
-import com.juliomachillanda.superheroes.domain.SuperHero;
-import com.juliomachillanda.superheroes.service.SuperHeroService;
+import com.juliomachillanda.superheroes.domain.Superhero;
+import com.juliomachillanda.superheroes.dto.SuperheroDTO;
+import com.juliomachillanda.superheroes.service.SuperheroService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/superheros")
-public class SuperHeroesController {
+@RequestMapping("/superheroes")
+public class SuperheroController {
 
     @Autowired
-    private SuperHeroService superHeroService;
+    private SuperheroService superheroService;
 
     @GetMapping
-    public List<SuperHero> findAll() {
-        return superHeroService.findAll();
+    public ResponseEntity<List<SuperheroDTO>> findAll() {
+        return ResponseEntity.ok(superheroService.findAll());
     }
 
     @PostMapping
-    public SuperHero create(@RequestBody SuperHero superHero) {
-        return superHeroService.create(superHero);
+    public ResponseEntity<SuperheroDTO> create(@RequestBody Superhero superhero) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(superheroService.create(superhero));
     }
 
     @PutMapping
-    public SuperHero update(@RequestBody SuperHero superHero) {
-        return superHeroService.update(superHero);
+    public ResponseEntity<SuperheroDTO> update(@RequestBody Superhero superhero) {
+        return ResponseEntity.ok(superheroService.update(superhero));
     }
 
     @GetMapping("/{id}")
-    public SuperHero findById(@PathVariable("id") Long id) {
-        return superHeroService.findById(id);
+    public ResponseEntity<SuperheroDTO> findById(@PathVariable("id") Long id) {
+        return ResponseEntity.ok(superheroService.findById(id));
     }
 
     @DeleteMapping("/{id}")
-    public void Delete(@PathVariable("id") Long id) {
-        superHeroService.delete(id);
+    public void delete(@PathVariable("id") Long id) {
+        superheroService.deleteById(id);
     }
 
-    @GetMapping("/service/{ch}")
-    public List<String> getBy(@PathVariable("ch") String ch) {
-        return superHeroService.getBy(ch);
+    @GetMapping("/search")
+    public ResponseEntity<List<SuperheroDTO>> getByNameContaining(@RequestParam("name") String name) {
+        return ResponseEntity.ok(superheroService.findByNameContaining(name));
     }
 
 }
